7e8cbd039522976199e06c864348095640d80534
ohyeah5566 <ohyeah5566@gmail.com>
2020-11-23 10:56:55 +0800

    couroutine stash

--- app/src/main/java/com/ohyeah5566/MainActivity.kt
+++ app/src/main/java/com/ohyeah5566/MainActivity.kt
@@ -2,10 +2,46 @@ package com.ohyeah5566
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
+    companion object {
+        val TAG = "MainActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        runBlocking(Dispatchers.IO) {
+            val listD = mutableListOf<Deferred<Int>>()
+            val listR = mutableListOf<Int>()
+            for (i in 0..10) {
+                listD.add(getNumbers(i))
+            }
+            listD.forEach {
+                listR.add(it.await())
+            }
+            listR.forEach {
+                Log.d(TAG, "it:$it")
+            }
+
+
+        }
     }
+
+    suspend fun getNumbers(input: Int) = async {
+        Log.d(TAG, "input:$input")
+        delay(input * 500L)
+        input
+    }
+
+
+    override val coroutineContext: CoroutineContext
+        get() = job
+    private val job = Job()
 }
