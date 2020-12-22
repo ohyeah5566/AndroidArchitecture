2b829fd6a86662d9b4e3a7ce3dc3cd36a8aba62a
ohyeah5566 <ohyeah5566@gmail.com>
2020-12-22 19:45:46 +0800

    刻搜尋amiibo的介面，spinner的初始化 顏色風格採用任天堂的配色 加入 recyclerView

--- app/src/main/java/com/ohyeah5566/MainActivity.kt
+++ app/src/main/java/com/ohyeah5566/MainActivity.kt
@@ -2,10 +2,37 @@ package com.ohyeah5566
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import com.ohyeah5566.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.search_type,
+            android.R.layout.simple_spinner_dropdown_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            binding.searchTypeSpinner.adapter = adapter
+        }
+
+        binding.searchTypeSpinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val type = parent?.getItemAtPosition(position) as String
+            }
+        }
     }
 }
