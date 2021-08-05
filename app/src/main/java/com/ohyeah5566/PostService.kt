8fdd51b022f87c23b32caf29c2936cfef1d29bcd
ohyeah5566 <ohyeah5566@gmail.com>
2021-08-05 11:35:24 +0800

    gradle java1.8 internet permission

--- app/src/main/java/com/ohyeah5566/PostService.kt
+++ app/src/main/java/com/ohyeah5566/PostService.kt
@@ -3,21 +3,27 @@ package com.ohyeah5566
 import com.ohyeah5566.model.Post
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface PostService {
-    @GET("gimme/{subReddit}")
+    @GET("gimme/{subReddit}/5")
     suspend fun getPost(@Path("subReddit") subReddit: String): BaseResponse<List<Post>>
 }
 
 fun getPostService(): PostService {
     val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+    val logInterceptor = HttpLoggingInterceptor()
+    logInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+    val client = OkHttpClient.Builder().addInterceptor(logInterceptor).build()
 
     return Retrofit.Builder()
         .baseUrl("https://meme-api.herokuapp.com/")
+        .client(client)
         .addConverterFactory(MoshiConverterFactory.create(moshi))
         .build()
         .create(PostService::class.java)
