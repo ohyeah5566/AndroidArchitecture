595202c8ef13e8ff7542c69762bb68f5540cdc44
ohyeah5566 <ohyeah5566@gmail.com>
2021-08-08 21:53:13 +0800

    composing build

---
+++ VersionPlugin/src/main/java/com/ohyeah5566/versionplugin/Deps.kt
@@ -0,0 +1,9 @@
+package com.ohyeah5566.versionplugin
+
+object Version{
+    const val recycler_view = "1.0.0"
+}
+
+object Deps {
+    const val recycler_view = "androidx.recyclerview:recyclerview:${Version.recycler_view}"
+}
---
+++ VersionPlugin/src/main/java/com/ohyeah5566/versionplugin/VersionPlugin.kt
@@ -0,0 +1,12 @@
+package com.ohyeah5566.versionplugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+
+class VersionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+
+    }
+
+}
